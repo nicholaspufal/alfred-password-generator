68a72f783bd0400bfbc7f403ff82220d28a81314
Nicholas Pufal <nicholas@petrofeed.com>
2016-01-01 16:39:25 -0200

    More secure passwords - bump to 15 the default length and use SecureRandom

--- src/random_password.rb
+++ src/random_password.rb
@@ -1,13 +1,20 @@
-result = ""
-length = (ARGV[0] || 10).to_i
+require 'securerandom'
 
-NUMBERS = (1..10).to_a
-UPPERCASE_LETTERS = ("A".."Z").to_a
-LOWERCASE_LETTERS = ("a".."z").to_a
-SYMBOLS = %w(^ ! ? & @)
+length = (ARGV[0] || 15).to_i
+
+NUMBERS = (0..9).to_a
+UPPERCASE_LETTERS = ('A'..'Z').to_a
+LOWERCASE_LETTERS = ('a'..'z').to_a
+SYMBOLS = %w(+ - ^ ! ? & @ \\)
 CHARS = NUMBERS + UPPERCASE_LETTERS + LOWERCASE_LETTERS + SYMBOLS
 
-max_number = CHARS.length
-length.times.each { result += CHARS[rand(max_number)].to_s }
+def random_character
+  random_index = SecureRandom.random_number(CHARS.length)
+  CHARS[random_index].to_s
+end
+
+result = length.times.inject('') do |result|
+  result += random_character
+end
 
 puts result
